d2d390d896ca1416792eed8e5573a804ae2ce2f3
cujohdum <stonefree112233@gmail.com>
2026-03-02 15:13:38 +0700

    Add new APIs

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.delete('/del', (req, res) => {
     res.send('<html><body>INSIDE DELETE API..</body></html>');
 });
 
+//REGISTER API
+app.post('/register', (req, res) => {
+    res.send('<html><body>INSIDE REGISTER API..</body></html>');
+});
+
+//PAYMENT API
+app.post('/payment', (req, res) => {
+    res.send('<html><body>INSIDE PAYMENT API..</body></html>');
+});
+
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(5000, () =>
     console.log('EXPRESS Server Started at Port No: 5000'));
